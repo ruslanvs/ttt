b8eb609f86bc61ce18330008d2c46eca4a92674d
ruslanvs <ruslanvs@yahoo.com>
2018-03-10 10:58:22 -0800

    Interim

--- ttt/ViewController.swift
+++ ttt/ViewController.swift
@@ -10,20 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    @IBOutlet weak var a1button: UIButton!
-    @IBOutlet weak var a2button: UIButton!
-    @IBOutlet weak var a3button: UIButton!
-    @IBOutlet weak var b1button: UIButton!
-    @IBOutlet weak var b2button: UIButton!
-    @IBOutlet weak var b3button: UIButton!
-    @IBOutlet weak var c1button: UIButton!
-    @IBOutlet weak var c2button: UIButton!
-    @IBOutlet weak var c3button: UIButton!
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var winnerLabel: UILabel!
     
     var c = UIColor.red
-    var score = [String: Int]()
-    var board = [Int: Int]()
+//    var score = [String: Int]()
+    var board = [Int: String]()
+    let combinations = [
+        [1,2,3], [4,5,6], [7,8,9],
+        [1,4,7], [2,5,8], [3,6,9],
+        [1,5,9], [3,5,7]
+    ]
 
     func color() -> UIColor {
 //        var color: UIColor = UIColor()
@@ -38,24 +35,33 @@ class ViewController: UIViewController {
     @IBAction func tttButtonPressed(_ sender: UIButton ){
         if sender.backgroundColor != UIColor.red && sender.backgroundColor != UIColor.blue {
             sender.backgroundColor = color()
+            if sender.backgroundColor == UIColor.red {
+                board[sender.tag] = "red"
+            } else {
+                board[sender.tag] = "blue"
+            }
+//            if let c = board[sender.tag] {
+//                print( board, c )
+//            }
+            for combination in combinations {
+                print( combination )
+                if board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]] {
+                    winnerLabel.text = board[combination[0]]
+                }
+            }
         }
     }
     
     @IBAction func reset(_ sender: UIButton ){
-        a1button.backgroundColor = UIColor.clear
-        a2button.backgroundColor = UIColor.clear
-        a3button.backgroundColor = UIColor.clear
-        b1button.backgroundColor = UIColor.clear
-        b2button.backgroundColor = UIColor.clear
-        b3button.backgroundColor = UIColor.clear
-        c1button.backgroundColor = UIColor.clear
-        c2button.backgroundColor = UIColor.clear
-        c3button.backgroundColor = UIColor.clear
+        for button in buttons {
+            button.backgroundColor = UIColor.clear
+        }
         
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        winnerLabel.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -63,8 +69,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-
 }
-
